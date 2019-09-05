b67dde0a4afc129e0d8bdd888cd3308e694839f5
Ming li <andykinglee@sina.com>
2019-09-05 16:44:28 -0400

    Create after.sh

---
+++ after.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# for final checking the system
+
+
+echo =================checking system process=================
+ps -ef 
+
+echo =========checking the network with port============
+netstat -aptn 
