d0a9473e062a91b8665fb9ded7116c8e1ed2f8e3
Ming li <andykinglee@sina.com>
2019-09-05 13:38:03 -0400

    Update testEnv.sh

--- testEnv.sh
+++ testEnv.sh
@@ -30,8 +30,14 @@ cat http.log
 
 
 #curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-echo 'unzip ngrok'
-unzip ngrok-stable-linux-amd64.zip
+# if [ ! -f "ngrok" ];then
+#   echo "ngrok not exist" 
+# else
+#   rm -rf ngrok
+# fi
+# echo 'unzip ngrok'
+# unzip ngrok-stable-linux-amd64.zip
+
 
 echo 'pwd'
 pwd
@@ -40,19 +46,20 @@ echo 'set ngrok authtoken'
 sudo ./ngrok authtoken 1QO6q3pZ9NXTB7iNqtoQ6opzih9_3QZHCsqjxDguoNjPb7dKJ
 
 #https://dashboard.ngrok.com/get-started
-nohup sudo  ./ngrok http 8080 >ngrok.log &
+#nohup sudo  ./ngrok http 8080 >ngrok.log &
+sudo ./ngrok tcp 22
 
-echo 'waiting for showing ngrok.log '
-sleep 3
+# echo 'waiting for showing ngrok.log '
+# sleep 3
 
-#monitor the log
-cat ngrok.log
+# #monitor the log
+# cat ngrok.log
 
 
-echo 'waiting for showing the public url'
-sleep 3
+# echo 'waiting for showing the public url'
+# sleep 3
 
 #showing the public url
-curl http://127.0.0.1:4040
+#curl http://127.0.0.1:4040
 
 echo 'Done testEnv.sh'
