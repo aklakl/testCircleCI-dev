55464dfdc612a64ff8ae6da66dafaf326d1d99bd
Ming li <andykinglee@sina.com>
2019-09-05 11:21:32 -0400

    Update testEnv.sh

--- testEnv.sh
+++ testEnv.sh
@@ -8,22 +8,26 @@ ifconfig -a
 
 #check current public IP  CircleCI docker container 3.92.182.196 
 curl 'https://api.ipify.org?format=json'
+echo '/n'
 
 #ipv6
 #curl 'https://api6.ipify.org?format=json'
 
 #sleep 5
 
-echo 'unzip ngrok'
+
 
 #curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-
+echo 'unzip ngrok'
 unzip ngrok-stable-linux-amd64.zip
 
+echo 'pwd'
 pwd
 
 echo 'set ngrok authtoken'
 
 sudo ./ngrok authtoken 1QO6q3pZ9NXTB7iNqtoQ6opzih9_3QZHCsqjxDguoNjPb7dKJ
 
-echo 'DOne testEnv.sh'
+
+
+echo 'Done testEnv.sh'
