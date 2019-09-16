3f112251fce50a3eff356e760608883b25f0c9fc
Ming li <andykinglee@sina.com>
2019-09-16 15:22:48 -0400

    Update installBaseTools.sh

--- script/sh/installBaseTools.sh
+++ script/sh/installBaseTools.sh
@@ -17,6 +17,7 @@ echo "============================ install base tools===========================
 #sudo sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
 #sudo apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 sudo apt-get update -y
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends apt-utils
 sudo apt-get install -y expect
 sudo apt-get install -y vim
 sudo apt-get install -y curl
