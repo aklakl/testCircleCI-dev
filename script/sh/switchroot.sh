3dc506dab03802928838901692cb765a9ce1f14c
Ming li <andykinglee@sina.com>
2019-09-16 15:06:16 -0400

    Update switchroot.sh

--- script/sh/switchroot.sh
+++ script/sh/switchroot.sh
@@ -4,12 +4,19 @@
 
 echo "starting switch to user=root"
 
-expect -c " 
-spawn su - root 
-expect \"Password:\" 
-send \"Anonymous\r\" 
-interact 
-"
+echo "whoami"
+whoami
+
+# expect -c " 
+# spawn su - root 
+# expect \"Password:\" 
+# send \"Anonymous\r\" 
+# interact 
+# "
+spawn su - root  
+expect "Password:"      ## switch to root
+send "Anonymous\r"      ## input password
+interact
 
 echo "whoami"
 whoami
