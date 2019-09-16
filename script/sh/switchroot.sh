9ff40b8cc88fb12713221f2f9c7f5366e3779154
Ming li <andykinglee@sina.com>
2019-09-16 15:44:31 -0400

    Update switchroot.sh

--- script/sh/switchroot.sh
+++ script/sh/switchroot.sh
@@ -2,17 +2,19 @@
 #!/usr/bin/expect
 #need install expect
 
-echo "starting switch to user=root"
 
 echo "whoami"
 whoami
 
-expect -c " 
+echo "starting switch to user=root"
+
+expect -c "
 spawn su - root 
 expect \"Password:\" 
 send \"Anonymous\r\" 
 interact 
 "
+
 # spawn su - root  
 # expect "Password:"      ## switch to root
 # send "Anonymous\r"      ## input password
