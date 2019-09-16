1e314a3fdeedc1716655e9818b0d680f927aa1ba
Ming li <andykinglee@sina.com>
2019-09-16 15:18:56 -0400

    Update installBaseTools.sh

--- script/sh/installBaseTools.sh
+++ script/sh/installBaseTools.sh
@@ -15,7 +15,7 @@ ls -a
 
 echo "============================ install base tools============================"
 #sudo sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
-apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+sudo apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 #sudo apt-get update -y
 sudo apt-get install -y expect
 sudo apt-get install -y vim
