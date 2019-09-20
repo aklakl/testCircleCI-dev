1cd27d15b48ef34701bb35079904091367336889
Ming li <andykinglee@sina.com>
2019-09-20 01:17:15 -0400

    Update recirculate.sh

--- script/sh/recirculate.sh
+++ script/sh/recirculate.sh
@@ -9,10 +9,14 @@ echo $j
 while true
 do
 	let "j=j+1";
-	let mod=$j%1000000;
+	let mod=$j%10000000;
 	#echo $mod;
 	if [ $mod == 0 ];then
-	  echo "----------j is $j--------------" 
+		echo "----------j is $j--------------" 
+		echo =================checking system process=================
+		ps -ef 
+		echo =========checking the network with port============
+		netstat -aptn 
 	fi
 	#sleep 1;  #debuging
 done
